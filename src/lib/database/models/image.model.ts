541749af573bf57dae99820d3db2d20bfcce390c
Marushchak Nazar <n.marushchak@knubisoft.com>
2024-04-15 17:45:56 +0300

    add all changes

--- src/lib/database/models/image.model.ts
+++ src/lib/database/models/image.model.ts
@@ -4,12 +4,12 @@ export interface IImage extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureUrl: string;
+  secureURL: string;
   width?: number;
   height?: number;
-  config?: Object;
+  config?: object;
   transformationUrl?: string;
-  aspecttRatio?: string;
+  aspectRatio?: string;
   color?: string;
   prompt?: string;
   author: {
@@ -18,18 +18,18 @@ export interface IImage extends Document {
     lastName: string;
   };
   createdAt?: Date;
-  updateAt?: Date;
+  updatedAt?: Date;
 }
 
 const ImageSchema = new Schema({
   title: { type: String, required: true },
   transformationType: { type: String, required: true },
-  publicId: { type: String, require: true },
-  secureUrl: { type: URL, required: true },
+  publicId: { type: String, required: true },
+  secureURL: { type: String, required: true },
   width: { type: Number },
   height: { type: Number },
   config: { type: Object },
-  transformationUrl: { type: URL },
+  transformationUrl: { type: String },
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
